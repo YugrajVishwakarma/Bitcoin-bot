b685550d0a596bef323bdf7c8aacef9e8e1a6982
Александр <162914939+zsluqa@users.noreply.github.com>
2024-03-10 22:52:37 +0400

    Telegram.cs

---
+++ Bitcoin Auto Withdraw/Network/Telegram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace BitcoinAuto.Withdraw
+{
+    public class Telegram
+    {
+        TelegramBotClient botClient;
+        string chatID = String.Empty;
+
+
+        public Telegram(string token, string chat_id)
+        {
+            this.botClient = new TelegramBotClient(token);
+            this.chatID = chat_id;
+        }
+
+
+        public async Task sendNotificationForNewTransaction()
+        {
+            await botClient.SendTextMessageAsync(chatID, "huy");
+        }
+
+        public async Task sendNotification(string message)
+        {
+            await botClient.SendTextMessageAsync(chatID, message);
+        }
+    }
+}
