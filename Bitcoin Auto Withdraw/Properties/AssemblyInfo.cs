ee161e3fb6355315f6f3829974848a033f49dd7e
Александр <162914939+zsluqa@users.noreply.github.com>
2024-03-10 22:52:38 +0400

    AssemblyInfo.cs

---
+++ Bitcoin Auto Withdraw/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bitcoin Auto Withdraw by unknown")]
+[assembly: AssemblyDescription("Automatic Withdrawal")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("by unknown")]
+[assembly: AssemblyProduct("Bitcoin Auto Withdraw")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8e9a8f9e-131a-4aad-adf8-92e4a60aad10")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
